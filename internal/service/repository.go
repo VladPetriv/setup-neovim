dc095bd43a01757b2cde1df7c0631d2d1af01b03
Vlad Petriv <vladpetriv05@gmail.com>
2022-11-25 22:09:01 +0200

    feat: add possibility to clone repository by ssh

--- internal/service/repository.go
+++ internal/service/repository.go
@@ -6,17 +6,37 @@ import (
 	"strings"
 
 	"github.com/go-git/go-git/v5"
+	"github.com/go-git/go-git/v5/plumbing/transport/ssh"
 )
 
 func (s service) CloneAndValidateRepository(url string) error {
-	if isContainsSshURL(url) {
-		return fmt.Errorf("sorry we don't support ssh urls")
-	}
-
-	_, err := git.PlainClone("nvim", false, &git.CloneOptions{
+	options := &git.CloneOptions{
 		URL:      url,
 		Progress: os.Stdout,
-	})
+	}
+
+	if isContainsSshURL(url) {
+		homeDir, err := os.UserHomeDir()
+		if err != nil {
+			return fmt.Errorf("failed to get home directory: %w", err)
+		}
+
+		keyPath, err := s.input.GetInput("Enter path to your ssh file(.ssh/id_ed3122)")
+		if err != nil {
+			return fmt.Errorf("failed to get user input: %w", err)
+		}
+
+		filePath := fmt.Sprintf("%s/%s", homeDir, keyPath)
+
+		publicKeys, err := ssh.NewPublicKeysFromFile("git", filePath, "")
+		if err != nil {
+			return fmt.Errorf("failed to create public keys from file: %w", err)
+		}
+
+		options.Auth = publicKeys
+	}
+
+	_, err := git.PlainClone("nvim", false, options)
 	if err != nil {
 		return fmt.Errorf("failed to clone repository: %w", err)
 	}
