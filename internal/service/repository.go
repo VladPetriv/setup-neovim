c9d2f201a9d1212857c7f08e47e77fd80b29ee44
Vlad Petriv <vladpetriv05@gmail.com>
2022-11-26 17:04:33 +0200

    refactor: move code block into function

--- internal/service/repository.go
+++ internal/service/repository.go
@@ -5,38 +5,27 @@ import (
 	"os"
 	"strings"
 
+	"github.com/VladPetriv/setup-neovim/pkg/input"
 	"github.com/go-git/go-git/v5"
 	"github.com/go-git/go-git/v5/plumbing/transport/ssh"
 )
 
 func (s service) CloneAndValidateRepository(url string) error {
-	options := &git.CloneOptions{
+	cloneOptions := &git.CloneOptions{ //nolint
 		URL:      url,
 		Progress: os.Stdout,
 	}
 
-	if isContainsSshURL(url) {
-		homeDir, err := os.UserHomeDir()
+	if haveSSHURLParts(url) {
+		publicKeys, err := createPublicSSHKeysFromFile(s.input)
 		if err != nil {
-			return fmt.Errorf("failed to get home directory: %w", err)
+			return fmt.Errorf("failed process ssh url: %w", err)
 		}
 
-		keyPath, err := s.input.GetInput("Enter path to your ssh file(.ssh/id_ed3122)")
-		if err != nil {
-			return fmt.Errorf("failed to get user input: %w", err)
-		}
-
-		filePath := fmt.Sprintf("%s/%s", homeDir, keyPath)
-
-		publicKeys, err := ssh.NewPublicKeysFromFile("git", filePath, "")
-		if err != nil {
-			return fmt.Errorf("failed to create public keys from file: %w", err)
-		}
-
-		options.Auth = publicKeys
+		cloneOptions.Auth = publicKeys
 	}
 
-	_, err := git.PlainClone("nvim", false, options)
+	_, err := git.PlainClone("nvim", false, cloneOptions)
 	if err != nil {
 		return fmt.Errorf("failed to clone repository: %w", err)
 	}
@@ -54,6 +43,29 @@ func (s service) CloneAndValidateRepository(url string) error {
 	return nil
 }
 
-func isContainsSshURL(url string) bool {
+func haveSSHURLParts(url string) bool {
 	return strings.Contains(url, "git@")
+}
+
+func createPublicSSHKeysFromFile(input input.Inputter) (*ssh.PublicKeys, error) {
+	homeDir, err := os.UserHomeDir()
+	if err != nil {
+		return nil, fmt.Errorf("failed to get home directory: %w", err)
+	}
+
+	fmt.Print("Enter path to your ssh file(.ssh/id_ed3122): ") //nolint
+
+	keyPath, err := input.GetInput()
+	if err != nil {
+		return nil, fmt.Errorf("failed to get user input: %w", err)
+	}
+
+	filePath := fmt.Sprintf("%s/%s", homeDir, keyPath)
+
+	publicKeys, err := ssh.NewPublicKeysFromFile("git", filePath, "")
+	if err != nil {
+		return nil, fmt.Errorf("failed to create ssh public keys from file: %w", err)
+	}
+
+	return publicKeys, nil
 }
