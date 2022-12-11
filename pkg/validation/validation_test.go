6aaf8fc22cb346872e258e067af2df245abb4dfc
Vlad Petriv <vladpetriv05@gmail.com>
2022-12-11 12:30:11 +0200

    test: add unit tests for repo file validation

--- pkg/validation/validation_test.go
+++ pkg/validation/validation_test.go
@@ -1,6 +1,7 @@
 package validation_test
 
 import (
+	"os"
 	"testing"
 
 	"github.com/VladPetriv/setup-neovim/pkg/validation"
@@ -41,6 +42,62 @@ func Test_ValidateURL(t *testing.T) {
 
 			err := testValidator.ValidateURL(tt.input)
 			assert.Equal(t, tt.expectedErr, err)
+		})
+	}
+}
+
+func Test_ValidateRepoFiles(t *testing.T) { //nolint:tparallel // t.Parallel() causes conflicts with directories
+	t.Parallel()
+
+	testValidator := validation.New()
+
+	tests := []struct {
+		name         string
+		input        string
+		withBaseFile bool
+		expectedErr  error
+	}{
+		{
+			name:         "validation for repository files successfully completed",
+			input:        "./nvim",
+			withBaseFile: true,
+			expectedErr:  nil,
+		},
+		{
+			name:        "validation for repository files failed with no required files",
+			input:       "./nvim",
+			expectedErr: validation.ErrNoBaseFilesInRepository,
+		},
+		{
+			name:        "validation for repository files failed with not found path",
+			input:       "./nvim_test",
+			expectedErr: validation.ErrDirectoryNotFound,
+		},
+	}
+
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			// NOTE: here is no t.parallel() because it create conflicts with creating directories
+			err := os.Mkdir("nvim", 0o755)
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			if tt.withBaseFile {
+				_, err = os.Create("./nvim/init.lua")
+				if err != nil {
+					t.Fatal(err)
+				}
+			}
+
+			err = testValidator.ValidateRepoFiles(tt.input)
+			assert.Equal(t, tt.expectedErr, err)
+
+			err = os.RemoveAll("./nvim")
+			if err != nil {
+				t.Fatal(err)
+			}
 		})
 	}
 }
