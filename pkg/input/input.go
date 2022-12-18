f6b9ada0149e0cdf6428984f6127b0cb4f456634
Vlad Petriv <vladpetriv05@gmail.com>
2022-12-18 13:13:32 +0200

    refactor: move nested input from manager package into input

--- pkg/input/input.go
+++ pkg/input/input.go
@@ -15,6 +15,27 @@ func New() Inputter {
 	return &input{}
 }
 
+func (i input) ProcessInputForPackageManagers(stdin io.Reader) (string, error) {
+	fmt.Print("Do you have any package managers installed?(y/n): ")
+
+	reader := bufio.NewReader(stdin)
+	haveInstalledPackageManagers, err := i.GetInput(reader)
+	if err != nil {
+		return "", fmt.Errorf("failed to get user input: %w", err)
+	}
+
+	switch haveInstalledPackageManagers {
+	case "y":
+		return "skip", nil
+	case "n":
+		fmt.Print("Choose package manager(packer/vim-plug): ")
+
+		return i.GetInput(reader)
+	default:
+		return "", nil
+	}
+}
+
 func (i input) GetInput(stdin io.Reader) (string, error) {
 	reader := bufio.NewReader(stdin)
 
