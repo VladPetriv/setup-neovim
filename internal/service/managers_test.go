fb7aadf69314af9e28f9833702ea7104c03a84c2
Vlad Petriv <vladpetriv05@gmail.com>
2022-12-18 13:14:46 +0200

    test: add unit tests for managers module

---
+++ internal/service/managers_test.go
@@ -0,0 +1,90 @@
+package service_test
+
+import (
+	"fmt"
+	"os"
+	"strings"
+	"testing"
+
+	"github.com/VladPetriv/setup-neovim/internal/service"
+	"github.com/VladPetriv/setup-neovim/pkg/input"
+	"github.com/VladPetriv/setup-neovim/pkg/validation"
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_ProcessPackageManagers(t *testing.T) { //nolint:tparallel // t.Parallel() causes conflicts with files
+	t.Parallel()
+
+	testService := service.New(&service.Options{
+		Inputter:  input.New(),
+		Validator: validation.New(),
+	})
+
+	tests := []struct {
+		name                   string
+		inputHaveInstalled     string
+		inputPackageManager    string
+		expectedPackageManager string
+		wantErr                bool
+		expectedError          error
+	}{
+		{
+			name:                   "ProcessPackageManagers with no needs to install package manager",
+			inputHaveInstalled:     "y",
+			expectedPackageManager: "",
+		},
+		{
+			name:                   "ProcessPackageManagers with installing packer",
+			inputHaveInstalled:     "n",
+			inputPackageManager:    "packer",
+			expectedPackageManager: "packer",
+		},
+		{
+			name:                   "ProcessPackageManagers with installing vim plug",
+			inputHaveInstalled:     "n",
+			inputPackageManager:    "vim-plug",
+			expectedPackageManager: "vim-plug",
+		},
+		{
+			name:               "ProcessPackageManagers with invalid input",
+			inputHaveInstalled: "",
+			wantErr:            true,
+			expectedError:      service.ErrEnterValidAnswer,
+		},
+
+		{
+			name:                "ProcessPackageManagers with invalid input",
+			inputHaveInstalled:  "n",
+			inputPackageManager: "hello",
+			wantErr:             true,
+			expectedError:       service.ErrEnterValidAnswer,
+		},
+	}
+
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			input := strings.NewReader(
+				fmt.Sprintf("%s\n%s\n", tt.inputHaveInstalled, tt.inputPackageManager),
+			)
+
+			got, err := testService.ProcessPackageManagers(input)
+			if tt.wantErr {
+				assert.Error(t, err)
+				assert.Equal(t, tt.expectedError, err)
+			} else {
+				assert.NoError(t, err)
+				assert.Equal(t, tt.expectedPackageManager, got)
+			}
+
+			if tt.expectedPackageManager == "packer" {
+				homeDir, _ := os.UserHomeDir()
+
+				err = os.RemoveAll(fmt.Sprintf("%s/.local/share/nvim/", homeDir))
+				if err != nil {
+					t.Fatal(err)
+				}
+			}
+		})
+	}
+}
