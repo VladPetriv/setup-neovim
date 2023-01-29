9b83d7306f02cc007662ca72d961cd2a3d99b6ef
Vlad Petriv <vladpetriv05@gmail.com>
2023-01-29 13:47:06 +0200

    style: fix  comment

--- pkg/errs/errs.go
+++ pkg/errs/errs.go
@@ -7,7 +7,7 @@ import (
 	"github.com/VladPetriv/setup-neovim/pkg/colors"
 )
 
-// WrapError used to print color error message and exit from program using os.Exit(1).
+// WrapError used to print color error message and exit from program using os.Exit(0).
 func WrapError(msg string, err error) {
 	colors.Red(msg)
 	colors.Red(fmt.Sprintf("Error: %v", err))
