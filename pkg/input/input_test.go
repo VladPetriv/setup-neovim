de54b3c54537cc4201e6b7b5c311d4c79f5eb2f4
Vlad Petriv <vladpetriv05@gmail.com>
2023-03-09 19:53:51 +0100

    refactor: remove business related logic from pkg layer

--- pkg/input/input_test.go
+++ pkg/input/input_test.go
@@ -3,7 +3,6 @@ package input_test
 import (
 	"bytes"
 	"fmt"
-	"strings"
 	"testing"
 
 	"github.com/VladPetriv/setup-neovim/pkg/input"
@@ -44,58 +43,6 @@ func Test_GetInput(t *testing.T) {
 				assert.NoError(t, err)
 				assert.EqualValues(t, tt.want, got)
 			}
-		})
-	}
-}
-
-func Test_ProcessInputForPackageManagers(t *testing.T) {
-	t.Parallel()
-
-	testInput := input.New()
-
-	tests := []struct {
-		name                   string
-		inputHaveInstalled     string
-		inputPackageManager    string
-		expectedPackageManager string
-	}{
-		{
-			name:                   "ProcessInputForPackageManagers with no needs to package manager",
-			inputHaveInstalled:     "y",
-			expectedPackageManager: "skip",
-		},
-		{
-			name:                   "ProcessInputForPackageManagers with packer",
-			inputHaveInstalled:     "n",
-			inputPackageManager:    "packer",
-			expectedPackageManager: "packer",
-		},
-		{
-			name:                   "ProcessInputForPackageManagers with vim-plug",
-			inputHaveInstalled:     "n",
-			inputPackageManager:    "vim-plug",
-			expectedPackageManager: "vim-plug",
-		},
-		{
-			name:                   "ProcessInputForPackageManagers with empty result",
-			inputHaveInstalled:     "n",
-			inputPackageManager:    "",
-			expectedPackageManager: "",
-		},
-	}
-
-	for _, tt := range tests {
-		tt := tt
-		t.Run(tt.name, func(t *testing.T) {
-			t.Parallel()
-
-			input := strings.NewReader(
-				fmt.Sprintf("%s\n%s\n", tt.inputHaveInstalled, tt.inputPackageManager),
-			)
-
-			got, err := testInput.ProcessInputForPackageManagers(input)
-			assert.NoError(t, err)
-			assert.Equal(t, tt.expectedPackageManager, got)
 		})
 	}
 }
