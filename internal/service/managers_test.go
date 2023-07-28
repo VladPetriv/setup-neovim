a4cf94d425cc3505a27d235bc28759d75e389e88
Vlad Petriv <vladpetriv05@gmail.com>
2023-07-28 20:52:26 +0200

    test(manager): cover detect package manager with unit tests

--- internal/service/managers_test.go
+++ internal/service/managers_test.go
@@ -2,6 +2,7 @@ package service_test
 
 import (
 	"fmt"
+	"os"
 	"strings"
 	"testing"
 
@@ -9,7 +10,87 @@ import (
 	"github.com/VladPetriv/setup-neovim/pkg/input"
 	"github.com/VladPetriv/setup-neovim/pkg/validation"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
+
+func TestManager_DetectInstalledPackageManagers(t *testing.T) {
+	homeDir, err := os.UserHomeDir()
+	require.NoError(t, err)
+
+	testService := service.New(&service.Options{
+		Inputter:  input.New(),
+		Validator: validation.New(),
+	})
+
+	type precondition struct {
+		createPackerDir  bool
+		createVimPlugDir bool
+	}
+
+	type expected struct {
+		count int
+	}
+
+	tests := []struct {
+		name         string
+		precondition precondition
+		expected     expected
+	}{
+		{
+			name: "DetectInstalledPackageManagers successfully with 2 detected package managers",
+			precondition: precondition{
+				createPackerDir:  true,
+				createVimPlugDir: true,
+			},
+			expected: expected{
+				count: 2,
+			},
+		},
+		{
+			name: "DetectInstalledPackageManagers successfully with 1 detected package managers",
+			precondition: precondition{
+				createPackerDir: true,
+			},
+			expected: expected{
+				count: 1,
+			},
+		},
+		{
+			name:     "DetectInstalledPackageManagers failed with no detected package managers",
+			expected: expected{},
+		},
+	}
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			err = os.RemoveAll(fmt.Sprintf("%s/.local/share/nvim/site/pack", homeDir))
+			assert.NoError(t, err)
+			err = os.RemoveAll(fmt.Sprintf("%s/.local/share/nvim/site/autoload", homeDir))
+			assert.NoError(t, err)
+
+			if tt.precondition.createPackerDir {
+				err = os.MkdirAll(fmt.Sprintf("%s/.local/share/nvim/site/pack", homeDir), 0o755)
+				require.NoError(t, err)
+			}
+			if tt.precondition.createVimPlugDir {
+				err = os.MkdirAll(fmt.Sprintf("%s/.local/share/nvim/site/autoload", homeDir), 0o755)
+				require.NoError(t, err)
+			}
+
+			t.Cleanup(func() {
+				os.RemoveAll(fmt.Sprintf("%s/.local/share/nvim/site/pack", homeDir))
+				assert.NoError(t, err)
+
+				err = os.RemoveAll(fmt.Sprintf("%s/.local/share/nvim/site/autoload", homeDir))
+				assert.NoError(t, err)
+			})
+
+			_, actualCount, actualErr := testService.DetectInstalledPackageManagers()
+			assert.NoError(t, actualErr)
+			assert.Equal(t, tt.expected.count, actualCount)
+		})
+	}
+}
 
 func TestManager_ProcessInputForPackageManagers(t *testing.T) {
 	t.Parallel()
