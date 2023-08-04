b981618fbc714680ed7738f9cee6133c4e94a2c0
Vlad Petriv <vladpetriv05@gmail.com>
2023-08-04 22:48:47 +0200

    refactor: implement repository srv, remove unecessary files

--- internal/service/repository.go
+++ internal/service/repository.go
@@ -13,16 +13,44 @@ import (
 	"github.com/go-git/go-git/v5/plumbing/transport/ssh"
 )
 
-func (s service) CloneAndValidateRepository(url string, stdin io.Reader) error {
+type repositoryService struct {
+	inputter  input.Inputter
+	validator validation.Validator
+}
+
+func NewRepository(inputter input.Inputter, validator validation.Validator) *repositoryService {
+	return &repositoryService{
+		inputter:  inputter,
+		validator: validator,
+	}
+}
+
+func (r repositoryService) ProcessUserURL(stdin io.Reader) (string, error) {
+	fmt.Print("Enter URL to your nvim config: ")
+
+	configURL, err := r.inputter.GetInput(stdin)
+	if err != nil {
+		return "", fmt.Errorf("get user input: %w", err)
+	}
+
+	err = r.validator.ValidateURL(configURL)
+	if err != nil {
+		return "", err
+	}
+
+	return configURL, nil
+}
+
+func (r repositoryService) CloneAndValidateRepository(url string, stdin io.Reader) error {
 	cloneOptions := &git.CloneOptions{
 		URL:      url,
 		Progress: os.Stdout,
 	}
 
 	if hasSSHURLParts(url) {
-		publicKeys, err := createPublicSSHKeysFromFile(s.input, stdin)
+		publicKeys, err := createPublicSSHKeysFromFile(r.inputter, stdin)
 		if err != nil {
-			return fmt.Errorf("failed to process ssh url: %w", err)
+			return fmt.Errorf("create public ssh key from file: %w", err)
 		}
 
 		cloneOptions.Auth = publicKeys
@@ -30,10 +58,10 @@ func (s service) CloneAndValidateRepository(url string, stdin io.Reader) error {
 
 	_, err := git.PlainClone("nvim", false, cloneOptions)
 	if err != nil {
-		return fmt.Errorf("failed to clone repository: %w", err)
+		return fmt.Errorf("clone repository: %w", err)
 	}
 
-	err = s.validator.ValidateRepoFiles("nvim")
+	err = r.validator.ValidateRepoFiles("nvim")
 	if err != nil {
 		removeErr := os.RemoveAll("nvim")
 		if removeErr != nil {
@@ -44,7 +72,7 @@ func (s service) CloneAndValidateRepository(url string, stdin io.Reader) error {
 			return err
 		}
 
-		return fmt.Errorf("repository validation failed: %w", err)
+		return fmt.Errorf("validate repository files: %w", err)
 	}
 
 	return nil
@@ -64,14 +92,14 @@ func createPublicSSHKeysFromFile(input input.Inputter, stdin io.Reader) (*ssh.Pu
 
 	keyPath, err := input.GetInput(stdin)
 	if err != nil {
-		return nil, fmt.Errorf("get input for ssh key path: %w", err)
+		return nil, fmt.Errorf("get user input: %w", err)
 	}
 
 	filePath := fmt.Sprintf("%s/%s", homeDir, keyPath)
 
 	publicKeys, err := ssh.NewPublicKeysFromFile("git", filePath, "")
 	if err != nil {
-		return nil, fmt.Errorf("create ssh public from file: %w", err)
+		return nil, fmt.Errorf("create ssh public keys: %w", err)
 	}
 
 	return publicKeys, nil
