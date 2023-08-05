34622fe68606819b32f5dc817c600bd794a2af92
Vlad Petriv <vladpetriv05@gmail.com>
2023-08-05 10:17:26 +0200

    refactor: remove huge service interface

--- internal/service/services.go
+++ internal/service/services.go
@@ -5,24 +5,10 @@ import (
 	"io"
 )
 
-type Services interface {
-	// CheckUtilStatus check if nvim and git are installed
-	CheckUtilStatus() map[string]string
-	// ProcessUserURL get URL by user input and validate them
-	ProcessUserURL(stdin io.Reader) (string, error)
-	// CloneAndValidateRepository clones git repository and check if repository have base files for nvim configuration
-	CloneAndValidateRepository(url string, stdin io.Reader) error
-	// InstallPackageManager ask user about which package manager to install
-	InstallPackageManager(stdin io.Reader) (string, error)
-	// ExtractAndMoveConfigDirectory get config directory from repository and move it to .config folder
-	ExtractAndMoveConfigDirectory(path string) error
-	// DeleteConfigOrStopInstallation checks if nvim config is exist and ask permission for deleting it.
-	DeleteConfigOrStopInstallation(stdin io.Reader) error
-	// DetectInstalledPackageManagers check if user has installed of any package managers.
-	DetectInstalledPackageManagers() (string, int, error)
-	// ProcessAlreadyInstalledPackageManagers is used to inform user about already installed managers
-	// and ask permission for deleting old and installing new.
-	ProcessAlreadyInstalledPackageManagers(countOfAlreadyInstalledManagers int, stdin io.Reader) (bool, error)
+type Services struct {
+	File       FileService
+	Manager    ManagerService
+	Repository RepositoryService
 }
 
 type ManagerService interface {
