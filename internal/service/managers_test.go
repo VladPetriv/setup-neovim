815485a6d13002ecfed61aa0972f38bc78edbbae
Vlad Petriv <vladpetriv05@gmail.com>
2026-04-05 18:35:08 +0200

    test(service): adjust test cases according to intefaces changes

--- internal/service/managers_test.go
+++ internal/service/managers_test.go
@@ -3,11 +3,9 @@ package service_test
 import (
 	"fmt"
 	"os"
-	"strings"
 	"testing"
 
 	"github.com/VladPetriv/setup-neovim/internal/service"
-	"github.com/VladPetriv/setup-neovim/pkg/input"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
@@ -16,52 +14,42 @@ func TestManager_DetectInstalledPackageManagers(t *testing.T) {
 	homeDir, err := os.UserHomeDir()
 	require.NoError(t, err)
 
-	testService := service.NewManager(input.New())
+	testService := service.NewManager()
 
 	type precondition struct {
 		createPackerDir  bool
 		createVimPlugDir bool
 	}
 
-	type expected struct {
-		count int
-	}
-
 	tests := []struct {
 		name         string
 		precondition precondition
-		expected     expected
+		wantCount    int
 	}{
 		{
-			name: "DetectInstalledPackageManagers successfully with 2 detected package managers",
+			name: "detects both package managers",
 			precondition: precondition{
 				createPackerDir:  true,
 				createVimPlugDir: true,
 			},
-			expected: expected{
-				count: 2,
-			},
+			wantCount: 2,
 		},
 		{
-			name: "DetectInstalledPackageManagers successfully with 1 detected package managers",
+			name: "detects one package manager",
 			precondition: precondition{
 				createPackerDir: true,
 			},
-			expected: expected{
-				count: 1,
-			},
+			wantCount: 1,
 		},
 		{
-			name:     "DetectInstalledPackageManagers failed with no detected package managers",
-			expected: expected{},
+			name:      "detects no package managers",
+			wantCount: 0,
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			err = os.RemoveAll(fmt.Sprintf("%s/.local/share/nvim/site/pack", homeDir))
-			assert.NoError(t, err)
-			err = os.RemoveAll(fmt.Sprintf("%s/.local/share/nvim/site/autoload", homeDir))
-			assert.NoError(t, err)
+			os.RemoveAll(fmt.Sprintf("%s/.local/share/nvim/site/pack", homeDir))
+			os.RemoveAll(fmt.Sprintf("%s/.local/share/nvim/site/autoload", homeDir))
 
 			if tt.precondition.createPackerDir {
 				err = os.MkdirAll(fmt.Sprintf("%s/.local/share/nvim/site/pack", homeDir), 0o755)
@@ -74,136 +62,41 @@ func TestManager_DetectInstalledPackageManagers(t *testing.T) {
 
 			t.Cleanup(func() {
 				os.RemoveAll(fmt.Sprintf("%s/.local/share/nvim/site/pack", homeDir))
-				assert.NoError(t, err)
-
-				err = os.RemoveAll(fmt.Sprintf("%s/.local/share/nvim/site/autoload", homeDir))
-				assert.NoError(t, err)
+				os.RemoveAll(fmt.Sprintf("%s/.local/share/nvim/site/autoload", homeDir))
 			})
 
-			_, actualCount, actualErr := testService.DetectInstalledPackageManagers()
-			assert.NoError(t, actualErr)
-			assert.Equal(t, tt.expected.count, actualCount)
+			detected, err := testService.DetectInstalledPackageManagers()
+			assert.NoError(t, err)
+			assert.Len(t, detected, tt.wantCount)
 		})
 	}
 }
 
-func TestManager_ProcessAlreadyInstalledPackageManagers(t *testing.T) {
+func TestManager_DeletePackageManagers(t *testing.T) {
 	homeDir, err := os.UserHomeDir()
 	require.NoError(t, err)
 
-	testService := service.NewManager(input.New())
+	testService := service.NewManager()
 
-	type precondition struct {
-		createPackerDir  bool
-		createVimPlugDir bool
-	}
+	packerDir := fmt.Sprintf("%s/.local/share/nvim/site/pack", homeDir)
+	vimPlugDir := fmt.Sprintf("%s/.local/share/nvim/site/autoload", homeDir)
 
-	type args struct {
-		count           int
-		wantRemoveInput string
-	}
+	err = os.MkdirAll(packerDir, 0o755)
+	require.NoError(t, err)
+	err = os.MkdirAll(vimPlugDir, 0o755)
+	require.NoError(t, err)
 
-	type expected struct {
-		shouldInstall bool
-		err           error
-	}
+	t.Cleanup(func() {
+		os.RemoveAll(packerDir)
+		os.RemoveAll(vimPlugDir)
+	})
 
-	tests := []struct {
-		name         string
-		precondition precondition
-		args         args
-		expected     expected
-	}{
-		{
-			name: "ProcessAlreadyInstalledPackageManagers successful with 2 deleted managers",
-			precondition: precondition{
-				createPackerDir:  true,
-				createVimPlugDir: true,
-			},
-			args: args{
-				count:           2,
-				wantRemoveInput: "y",
-			},
-			expected: expected{
-				shouldInstall: true,
-			},
-		},
-		{
-			name: "ProcessAlreadyInstalledPackageManagers successful with 1 deleted managers",
-			precondition: precondition{
-				createPackerDir: true,
-			},
-			args: args{
-				count:           1,
-				wantRemoveInput: "y",
-			},
-			expected: expected{
-				shouldInstall: true,
-			},
-		},
-		{
-			name: "ProcessAlreadyInstalledPackageManagers successful with no detected managers",
-			precondition: precondition{
-				createPackerDir: true,
-			},
-			args: args{
-				count: 0,
-			},
-			expected: expected{
-				shouldInstall: true,
-			},
-		},
-		{
-			name: "ProcessAlreadyInstalledPackageManagers failed with no need to delete",
-			precondition: precondition{
-				createPackerDir: true,
-			},
-			args: args{
-				count:           1,
-				wantRemoveInput: "n",
-			},
-			expected: expected{
-				shouldInstall: false,
-				err:           nil,
-			},
-		},
-		{
-			name: "ProcessAlreadyInstalledPackageManagers failed with invalid answer",
-			precondition: precondition{
-				createPackerDir: true,
-			},
-			args: args{
-				count:           1,
-				wantRemoveInput: "invalid",
-			},
-			expected: expected{
-				err: service.ErrEnterValidAnswer,
-			},
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			err = os.RemoveAll(fmt.Sprintf("%s/.local/share/nvim/site/pack", homeDir))
-			assert.NoError(t, err)
-			err = os.RemoveAll(fmt.Sprintf("%s/.local/share/nvim/site/autoload", homeDir))
-			assert.NoError(t, err)
+	err = testService.DeletePackageManagers()
+	assert.NoError(t, err)
 
-			if tt.precondition.createPackerDir {
-				err = os.MkdirAll(fmt.Sprintf("%s/.local/share/nvim/site/pack", homeDir), 0o755)
-				require.NoError(t, err)
-			}
-			if tt.precondition.createVimPlugDir {
-				err = os.MkdirAll(fmt.Sprintf("%s/.local/share/nvim/site/autoload", homeDir), 0o755)
-				require.NoError(t, err)
-			}
+	_, err = os.Lstat(packerDir)
+	assert.Error(t, err, "packer directory should have been deleted")
 
-			input := strings.NewReader(
-				fmt.Sprintf("%s\n", tt.args.wantRemoveInput),
-			)
-
-			actual, processErr := testService.ProcessAlreadyInstalledPackageManagers(tt.args.count, input)
-			assert.Equal(t, tt.expected.err, processErr)
-			assert.Equal(t, tt.expected.shouldInstall, actual)
-		})
-	}
+	_, err = os.Lstat(vimPlugDir)
+	assert.Error(t, err, "vim-plug directory should have been deleted")
 }
